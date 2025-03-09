fddfdfd2db3cccab357b767d83f765b8b13e6a2b
Hrebynakha Anatolii <hrebynakha@gmail.com>
2025-03-09 15:11:09 +0200

    added Jinja template html file

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import pathlib
 import mimetypes
 import json
 from datetime import datetime
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 STORAGE_PATH = "storage/data.json"
+
 
 class JsonDataHandler:
     """Class for work with json as pickle"""
@@ -30,6 +31,21 @@ class JsonDataHandler:
         return loaded_data
 
 
+class HtmlTemplate:
+    """Html template reneder using Jinja2"""
+
+    def __init__(self, template_path):
+        self.env = Environment(loader=FileSystemLoader("."))
+        self.template = self.env.get_template(template_path)
+
+    def render_bytes(self, **kwargs) -> bytes:
+        """Return html template as encoded utf-8 bytes"""
+        output = self.template.render(
+            **kwargs,
+        )
+        return output.encode("utf-8")
+
+
 class HttpHandler(BaseHTTPRequestHandler):
     """HttpHandler"""
 
@@ -40,8 +56,10 @@ class HttpHandler(BaseHTTPRequestHandler):
             self.send_html_file("index.html")
         elif pr_url.path == "/message":
             self.send_html_file("message.html")
-        elif pr_url.path == "/read":
+        elif pr_url.path == "/read" and pr_url.query == "format=json":
             self.send_json(STORAGE_PATH)
+        elif pr_url.path == "/read":
+            self.send_template("output.html", filename=STORAGE_PATH)
         else:
             if pathlib.Path().joinpath(pr_url.path[1:]).exists():
                 self.send_static()
@@ -67,7 +85,9 @@ class HttpHandler(BaseHTTPRequestHandler):
         with open(filename, "rb") as fd:
             self.wfile.write(fd.read())
 
-    def send_static(self):
+    def send_static(
+        self,
+    ):
         """Process static files"""
         self.send_response(200)
         mt = mimetypes.guess_type(self.path)
@@ -81,27 +101,20 @@ class HttpHandler(BaseHTTPRequestHandler):
 
     def send_json(self, filename, status=200):
         """Process json files"""
-        json_handler = JsonDataHandler(filename)
-        data = json_handler.load_data()
-        rows_tmp = Template(
-            """
-        <h1><a href="/">Home</a></h1>
-        {% if data %}
-            {% for timestamp, info in data.items() %}
-            <li>{{ timestamp }}: {{info['username']}} {{info['message']}}</li>
-            {% endfor %}
-        {% else %}
-        <h2>Not messages yet.</h2>
-        {% endif %}
-        """
-        )
+        self.send_response(status)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        with open(filename, "rb") as fd:
+            self.wfile.write(fd.read())
+
+    def send_template(self, template_path, data=None, filename=None, status=200):
+        """Send html file as Jinja2 template with data"""
+        if not data:
+            data = JsonDataHandler(filename).load_data()
         self.send_response(status)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        output = rows_tmp.render(
-            data=data,
-        )
-        self.wfile.write(output.encode(encoding="utf-8"))
+        self.wfile.write(HtmlTemplate(template_path).render_bytes(data=data))
 
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
